0bb6bcfb7f9e2b518ba746c88f56606d630a14c3
alikanan <a.kanan2222@gmail.com>
2023-02-20 23:35:44 +0330

    add-functional-skills-list

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,18 @@
 /*=============== FILTERS TABS ===============*/
+const tabs = document.querySelector('[data-target]'),
+      tabContents = document.querySelector('[data-content]')
 
-
+tabs.forEach(tab => {
+    tab.addEventListener('click',() =>{
+        const target = document.querySelector(tab.dataset.target)
+        tabContents.forEach(tc => { 
+            tc.classList.remove('filters__active')
+        })
+        target.classList.add('filters__active')
+    })
+});
 /*=============== DARK LIGHT THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+fun
