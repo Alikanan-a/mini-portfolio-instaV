0b32c7e437d2376993a18b0784a9187ab48382ee
alikanan <a.kanan2222@gmail.com>
2023-02-22 00:53:38 +0330

    add-jquery--fix-bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,46 @@
-/*=============== FILTERS TABS ===============*/
-const tabs = document.querySelector('[data-target]'),
-      tabContents = document.querySelector('[data-content]')
+// /*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
 
 tabs.forEach(tab => {
     tab.addEventListener('click',() =>{
         const target = document.querySelector(tab.dataset.target)
-        tabContents.forEach(tc => { 
+
+        tabContents.forEach(tc => {
             tc.classList.remove('filters__active')
         })
         target.classList.add('filters__active')
+        tabs.forEach(t => {
+          t.classList.remove('filter-tab-active')
+        })
+        tab.classList.add('filter-tab-active')
     })
 });
-/*=============== DARK LIGHT THEME ===============*/
+// <!--=============== Jquery ===============-->
+// $(function () {
+//   let project = true;
+//   let skills = false;
+//   $("#project").on("click", function () {
+//     if (!project) {
+//       $(".project-content").fadeIn();
+//       $(".skill-content").fadeOut();
+//       project = true;
+//       skills = false;
+//     }
+//   });
+//   $("#skills").on("click", function () {
+//     if (!skills) {
+//       $(".skill-content").fadeIn();
+//       $(".project-content").fadeOut();
+//       project = false;
+//       skills = true;
+//     }
+//   });
+// });
+
+// /*=============== DARK LIGHT THEME ===============*/
+
+// /*=============== SCROLL REVEAL ANIMATION ===============*/
 
 
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+
