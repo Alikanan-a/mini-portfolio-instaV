3ed9f6dbfc059cbce4f7ee8d2fa9eb24cc03f011
alikanan <a.kanan2222@gmail.com>
2023-03-01 17:43:11 +0330

    add-login-page-fix-js-add-responsive-color-buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,126 @@
 // /*=============== FILTERS TABS ===============*/
 const tabs = document.querySelectorAll('[data-target]'),
-      tabContents = document.querySelectorAll('[data-content]')
+  tabContents = document.querySelectorAll('[data-content]')
 
 tabs.forEach(tab => {
-    tab.addEventListener('click',() =>{
-        const target = document.querySelector(tab.dataset.target)
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
 
-        tabContents.forEach(tc => {
-            tc.classList.remove('filters__active')
-        })
-        target.classList.add('filters__active')
-        tabs.forEach(t => {
-          t.classList.remove('filter-tab-active')
-        })
-        tab.classList.add('filter-tab-active')
+    tabContents.forEach(tc => {
+      tc.classList.remove('filters__active')
     })
+    target.classList.add('filters__active')
+    tabs.forEach(t => {
+      t.classList.remove('filter-tab-active')
+    })
+    tab.classList.add('filter-tab-active')
+  })
 });
 // <!--=============== colors ===============-->
-const Purple = document.querySelector('.menu > .toggle:checked ~ .tab:nth-of-type(2)')
-const Pink = document.querySelector('.menu > .toggle:checked ~ .tab:nth-of-type(2)')
-const Teal = document.querySelector('.menu > .toggle:checked ~ .tab:nth-of-type(2)')
-const red = document.querySelector('.menu > .toggle:checked ~ .tab:nth-of-type(2)')
-const orange = document.querySelector('.menu > .toggle:checked ~ .tab:nth-of-type(2)')
-const brown = document.querySelector('.menu > .toggle:checked ~ .tab:nth-of-type(2)')
-const lightBlue = document.querySelector('.menu > .toggle:checked ~ .tab:nth-of-type(2)')
+const blue = document.querySelector('#blue')
+const purple = document.querySelector('#purple')
+const pink = document.querySelector('#pink')
+const teal = document.querySelector('#teal')
+const red = document.querySelector('#red')
+const orange = document.querySelector('#orange')
+const brown = document.querySelector('#brown')
+const gray = document.querySelector('#gray')
+const body = document.querySelector('body')
+
+
+$(function () {
+  $(window).on('beforeunload', function(){
+    $(window).scrollTop(0);
+  });
+  $('#purple').click(function (e) {
+    e.preventDefault()
+    $("body").removeClass('blue');
+    $("body").removeClass('pink');
+    $("body").removeClass('teal');
+    $("body").removeClass('red');
+    $("body").removeClass('orange');
+    $("body").removeClass('brown');
+    $("body").removeClass('gray');
+    $('body').addClass('purple')
+  })
+  $('#blue').click(function (e) {
+    e.preventDefault()
+    $("body").removeClass('purple');
+    $("body").removeClass('pink');
+    $("body").removeClass('teal');
+    $("body").removeClass('red');
+    $("body").removeClass('orange');
+    $("body").removeClass('brown');
+    $("body").removeClass('gray');
+    $('body').addClass('blue')
+  })
+  $('#pink').click(function (e) {
+    e.preventDefault()
+    $("body").removeClass('purple');
+    $("body").removeClass('blue');
+    $("body").removeClass('teal');
+    $("body").removeClass('red');
+    $("body").removeClass('orange');
+    $("body").removeClass('brown');
+    $("body").removeClass('gray');
+    $('body').addClass('pink')
+  })
+  $('#teal').click(function (e) {
+    e.preventDefault()
+    $("body").removeClass('purple');
+    $("body").removeClass('pink');
+    $("body").removeClass('blue');
+    $("body").removeClass('red');
+    $("body").removeClass('orange');
+    $("body").removeClass('brown');
+    $("body").removeClass('gray');
+    $('body').addClass('teal')
+  })
+  $('#red').click(function (e) {
+    e.preventDefault()
+    $("body").removeClass('purple');
+    $("body").removeClass('pink');
+    $("body").removeClass('teal');
+    $("body").removeClass('blue');
+    $("body").removeClass('orange');
+    $("body").removeClass('brown');
+    $("body").removeClass('gray');
+    $('body').addClass('red')
+  })
+  $('#orange').click(function (e) {
+    e.preventDefault()
+    $("body").removeClass('purple');
+    $("body").removeClass('pink');
+    $("body").removeClass('teal');
+    $("body").removeClass('red');
+    $("body").removeClass('blue');
+    $("body").removeClass('brown');
+    $("body").removeClass('gray');
+    $('body').addClass('orange')
+  })
+  $('#brown').click(function (e) {
+    e.preventDefault()
+    $("body").removeClass('purple');
+    $("body").removeClass('pink');
+    $("body").removeClass('teal');
+    $("body").removeClass('red');
+    $("body").removeClass('orange');
+    $("body").removeClass('blue');
+    $("body").removeClass('gray');
+    $('body').addClass('brown')
+  })
+  $('#gray').click(function (e) {
+    e.preventDefault()
+    $("body").removeClass('purple');
+    $("body").removeClass('pink');
+    $("body").removeClass('teal');
+    $("body").removeClass('red');
+    $("body").removeClass('orange');
+    $("body").removeClass('brown');
+    $("body").removeClass('blue');
+    $('body').addClass('gray')
+  })
+})
 // <!--=============== Jquery ===============-->
 // $(function () {
 //   let project = true;
@@ -68,36 +165,51 @@ if (selectedTheme) {
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
 })
 // /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
-  origin : 'top',
-  distance : '60px',
-  duration : 2500 , 
-  delay : 400 ,
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
 })
 
 sr.reveal('.profile__border')
-sr.reveal('.profile__name'  , {delay:500})
-sr.reveal('.profile__profession'  , {delay:600})
-sr.reveal('.profile__social'  , {delay:700})
-sr.reveal('.profile__info-group' , {interval: 100,delay:700})
-sr.reveal('.profile__buttons'  , {delay:800})
-sr.reveal('.filters__content'  , {delay:900})
-sr.reveal('.filters'  , {delay:1000})
-  
+sr.reveal('.profile__name', {
+  delay: 500
+})
+sr.reveal('.profile__profession', {
+  delay: 600
+})
+sr.reveal('.profile__social', {
+  delay: 700
+})
+sr.reveal('.profile__info-group', {
+  interval: 100,
+  delay: 700
+})
+sr.reveal('.profile__buttons', {
+  delay: 800
+})
+sr.reveal('.filters__content', {
+  delay: 900
+})
+sr.reveal('.filters', {
+  delay: 1000
+})
+
 var typingEffect = new Typed(".multiText", {
-  strings :['architecture','Designer', 'house-designer'],
+  strings: ['architecture', 'Designer', 'house-designer'],
   loop: true,
-  typeSpeed :100,
-  backSpeed:80,
-  backDelay:1500
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1500
 })
 
 
