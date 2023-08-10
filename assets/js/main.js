0d3b0756c9f5aa0437d26b4fa374ffd3490e690a
alikanan <a.kanan2222@gmail.com>
2023-08-10 18:48:48 +0430

    add modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -213,6 +213,31 @@ var typingEffect = new Typed(".multiText", {
 })
 
 
+const openButton = document.querySelector('#open-btn')
+const closeButton = document.querySelector('#close-btn')
+const modalContainer = document.querySelector('#modal-container')
+
+openButton.addEventListener('click',function(){
+
+    modalContainer.style.display = 'block';
+    
+})
+
+closeButton.addEventListener('click', () => {
+
+    modalContainer.style.display = 'none';
+
+})
+
+window.addEventListener('click', (e)=> {
+
+    if(e.target === modalContainer){
+
+        modalContainer.style.display = 'none';
+
+    }
+    //console.log(e.target)
+})
 //   document.getElementById("errorMsg").style.display = "none";
 // function validateForm() {
 //   let x = document.forms["myForm"]["fname"].value;
